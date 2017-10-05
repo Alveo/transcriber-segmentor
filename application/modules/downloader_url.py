ee5e7ef1640ab596d355edfa39b72fd51a1257f1
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-05 11:26:52 +1100

    Initial commit

---
+++ application/modules/downloader_url.py
@@ -0,0 +1,17 @@
+import shutil
+import urllib.request
+import urllib.error
+
+def download(url, filename):
+    """ Downloads a file from the specified URL to the specified destination.
+
+    Returns a HTTP status code. If it is not 200, it would be a safe assumption to say the download failed. """
+    exit_code = 200
+
+    try:
+        with urllib.request.urlopen(url) as response, open(filename, 'wb') as file_handle:
+            shutil.copyfileobj(response, file_handle)
+    except urllib.error.HTTPError as exception:
+        exit_code = exception.code
+
+    return exit_code
