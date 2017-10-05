01f040aef80ee267b95c0d830906a345ea3f285f
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-05 14:34:53 +1100

    Added partial POST file upload support
    Removed non-Alveo domain URL requirement
    Fixed error code undefined exit

--- application/routes.py
+++ application/routes.py
@@ -5,6 +5,7 @@ from flask import redirect, jsonify, request
 from application import app
 from application.modules.error_views import not_allowed, not_found, server_error
 from application.modules.downloader_url import URLDownloader
+from application.modules.downloader_post import PostDownloader
 from application.modules.audio_segment import AudioSegmentor
 
 app.register_error_handler(403, not_allowed)
@@ -20,31 +21,57 @@ def serve():
     return "Index"
 
 @app.route('/api/segment/url')
-def url_download():
-    status = "An unknown error occurred."
-
+def segment_url():
+    status = ""
     url = request.args.get('url', default="", type=str)
 
     filename = app.config['DOWNLOAD_CACHE_PATH'] + str(uuid.uuid4())
     downloader = URLDownloader(url, filename)
 
     if downloader.isValid():
-        if not downloader.isAlveo():
-            status = downloader.download();
+        status = downloader.download();
 
-            # Confirm the file type is audio
-            if status == 200:
-                processor = AudioSegmentor(filename)
-                if processor.isValid():
-                    status = jsonify(processor.segment())
-                else: 
-                    status = "{error: \"Specified URL did not contain a valid .wav audio file.\"}"
-            else:
-                status = "{error: \"Resource returned error code "+str(status)+"\".}"
+        # Confirm the file type is audio
+        if status == 200:
+            processor = AudioSegmentor(filename)
+            if processor.isValid():
+                status = jsonify(processor.segment())
+            else: 
+                status = "{error: \"Specified URL did not contain a valid .wav audio file.\"}"
         else:
-            status = "{error: \"Alveo URLs are currently not supported.\"}"
+            status = "{error: \"Resource returned error code "+str(status)+"\".}"
     else:
         status = "{error: \"URL pattern is not valid. Example: http://example.com/\"}"
 
+    if status is "":
+        status = "An unknown error occurred."
+
     downloader.cleanup()
+
+    return status
+
+@app.route('/api/segment/upload', methods=['POST'])
+def segment_upload():
+    status = ""
+    if 'file' in request.files:
+        filedata = request.files['file']
+        if filedata.filename is not "":
+            filename = app.config['DOWNLOAD_CACHE_PATH'] + str(uuid.uuid4())
+
+            downloader = PostDownloader(filename, filedata)
+            downloader.save()
+
+            # Run segmentor.isValid
+            # Segment
+            status = "Success"
+        else:
+            status = "{error: \"No file selected in query.\"}"
+    else:
+        status = "{error: \"No file attached to query.\"}"
+
+    if status is "":
+        status = "An unknown error occurred."
+
+    downloader.cleanup()
+
     return status
