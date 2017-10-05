d33695969356d86883ddfe2644f11162f4c173c6
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-05 12:07:39 +1100

    Converted to object, added Alveo URL detection

--- application/modules/downloader_url.py
+++ application/modules/downloader_url.py
@@ -3,26 +3,51 @@ import urllib.request
 import urllib.error
 import urllib.parse
 
-def isValid(url):
-    valid = False
+# TODO config
+alveo_paths = ['alveo.edu.au',
+                'www.alveo.edu.au',
+                'staging.alveo.edu.au']
 
-    schema = urllib.parse.urlparse(url)
+class URLDownloader:
+    def __init__(self, url):
+        self.url = url
+        
+        self.validURL = False
+        self.alveoURL = False
 
-    if schema.scheme != '' and schema.netloc != '':
-        valid = True
+        self._validate()
 
-    return valid
+    def _validate(self):
+        """ Validates whether the URL is both valid and whether it is a URL that needs an API library to access. Should not be called from outside the class. """
+        schema = urllib.parse.urlparse(self.url)
 
-def download(url, filename):
-    """ Downloads a file from the specified URL to the specified destination.
+        self.validURL = False
+        self.alveoURL = False
 
-    Returns a HTTP status code. If it is not 200, it would be a safe assumption to say the download failed. """
-    exit_code = 200
+        if schema.scheme is not '' and schema.netloc is not '':
+            self.validURL = True
 
-    try:
-        with urllib.request.urlopen(url) as response, open(filename, 'wb') as file_handle:
-            shutil.copyfileobj(response, file_handle)
-    except urllib.error.HTTPError as exception:
-        exit_code = exception.code
+        if schema.netloc in alveo_paths:
+            self.alveoURL = True
 
-    return exit_code
+    def isValid(self):
+        """ Returns true if the URL protocol and address is valid. """
+        return self.validURL
+
+    def isAlveo(self):
+        """ Returns true if the address matches an Alveo domain. """
+        return self.alveoURL
+
+    def download(self, filename):
+        """ Downloads a file from the specified URL to the specified destination.
+
+        Returns a HTTP status code. If it is not 200, it would be a safe assumption to say the download failed. """
+        exit_code = 200
+
+        try:
+            with urllib.request.urlopen(self.url) as response, open(filename, 'wb') as file_handle:
+                shutil.copyfileobj(response, file_handle)
+        except urllib.error.HTTPError as exception:
+            exit_code = exception.code
+
+        return exit_code
