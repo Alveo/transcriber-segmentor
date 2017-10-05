bcababd4a51c714a8e9a56217e8b0b38a0e28856
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-05 12:28:24 +1100

    Support global config, directory creation

--- application/modules/downloader_url.py
+++ application/modules/downloader_url.py
@@ -1,12 +1,10 @@
+import os
 import shutil
 import urllib.request
 import urllib.error
 import urllib.parse
 
-# TODO config
-alveo_paths = ['alveo.edu.au',
-                'www.alveo.edu.au',
-                'staging.alveo.edu.au']
+from application import app
 
 class URLDownloader:
     def __init__(self, url):
@@ -15,9 +13,9 @@ class URLDownloader:
         self.validURL = False
         self.alveoURL = False
 
-        self._validate()
+        self._validateURL()
 
-    def _validate(self):
+    def _validateURL(self):
         """ Validates whether the URL is both valid and whether it is a URL that needs an API library to access. Should not be called from outside the class. """
         schema = urllib.parse.urlparse(self.url)
 
@@ -27,7 +25,7 @@ class URLDownloader:
         if schema.scheme is not '' and schema.netloc is not '':
             self.validURL = True
 
-        if schema.netloc in alveo_paths:
+        if schema.netloc in app.config['ALVEO_DOMAINS']:
             self.alveoURL = True
 
     def isValid(self):
@@ -38,11 +36,18 @@ class URLDownloader:
         """ Returns true if the address matches an Alveo domain. """
         return self.alveoURL
 
+    def _validate_path(self, filename):
+        directory = os.path.dirname(os.path.realpath(filename))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
     def download(self, filename):
         """ Downloads a file from the specified URL to the specified destination.
 
         Returns a HTTP status code. If it is not 200, it would be a safe assumption to say the download failed. """
         exit_code = 200
+
+        self._validate_path(filename)
 
         try:
             with urllib.request.urlopen(self.url) as response, open(filename, 'wb') as file_handle:
