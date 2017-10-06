095377fb52bcf8f578047aedaf5ebd3bce3e4444
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-06 17:39:51 +1100

    Add global config for cross origin

--- application/__init__.py
+++ application/__init__.py
@@ -6,13 +6,11 @@ BASE_FOLDER = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 app.static_folder = os.path.join(BASE_FOLDER, 'static')
 app.config.from_pyfile('../config')
 
-# Allow cross origin headers only on dev mode
-if app.config['DEVELOPMENT'] == True:
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-        return response
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', app.config['ACCESS_CONTROL_ALLOW_ORIGIN'])
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST')
+    return response
 
 import application.routes
