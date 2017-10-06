7f35b94b2aa8ce7abe249f622e0cda32a48942e7
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-06 18:10:43 +1100

    Support disabling file POST

--- application/routes.py
+++ application/routes.py
@@ -51,34 +51,35 @@ def segment_url():
     downloader.cleanup()
     return status
 
-@app.route('/api/segment/upload', methods=['POST'])
-def segment_upload():
-    """ Segments a file receied via POST from a client. Returns JSON if successful, else an error message. """
+if app.config['ALLOW_FILE_POST']:
+    @app.route('/api/segment/upload', methods=['POST'])
+    def segment_upload():
+        """ Segments a file receied via POST from a client. Returns JSON if successful, else an error message. """
 
-    status = ""
-    if 'file' in request.files:
-        filedata = request.files['file']
-        if filedata.filename is not "":
-            filename = app.config['DOWNLOAD_CACHE_PATH'] + str(uuid.uuid4())
+        status = ""
+        if 'file' in request.files:
+            filedata = request.files['file']
+            if filedata.filename is not "":
+                filename = app.config['DOWNLOAD_CACHE_PATH'] + str(uuid.uuid4())
 
-            # Save the file for audiosegmentor
-            downloader = PostDownloader(filename, filedata)
-            downloader.save()
+                # Save the file for audiosegmentor
+                downloader = PostDownloader(filename, filedata)
+                downloader.save()
 
-            # Attempt to segment the file
-            processor = AudioSegmentor(filename)
-            if processor.isValid():
-                # Return pure JSON to the client
-                status = jsonify(processor.segment())
-            else: 
-                status = "{error: \"Uploaded file is not a valid .wav audio file.\"}"
+                # Attempt to segment the file
+                processor = AudioSegmentor(filename)
+                if processor.isValid():
+                    # Return pure JSON to the client
+                    status = jsonify(processor.segment())
+                else: 
+                    status = "{error: \"Uploaded file is not a valid .wav audio file.\"}"
+            else:
+                status = "{error: \"No file selected in query.\"}"
         else:
-            status = "{error: \"No file selected in query.\"}"
-    else:
-        status = "{error: \"No file attached to query.\"}"
+            status = "{error: \"No file attached to query.\"}"
 
-    if status is "":
-        status = "{error: \"An unknown error occurred.\"}"
+        if status is "":
+            status = "{error: \"An unknown error occurred.\"}"
 
-    downloader.cleanup()
-    return status
+        downloader.cleanup()
+        return status
