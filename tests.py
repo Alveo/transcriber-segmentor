ebbe0a81e8af4e7536beae36f5e63c8458ccf8ad
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-06 20:29:49 +1100

    Use dict testing, test generic on/off

--- tests.py
+++ tests.py
@@ -6,40 +6,55 @@ class Tester(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
+        self.url_pattern_fail = "URL pattern is either not valid"
+        self.url_connection_fail = "Errno"
+
+        self.attachments_generic_urls = [
+                {
+                    'expect': self.url_pattern_fail,
+                    'items': [
+                        '?',
+                        '?url=',
+                        '?url=',
+                        '',
+                        '?/file/'
+                    ]
+                },
+                {
+                    'expect': self.url_connection_fail,
+                    'items': [
+                        '?url=http://somedomainthatdoesnexit/file',
+                        '?url=http://somedomainthatdoesnexit.asdf/file',
+                        '?url=http://127.0.0.55/file',
+                        '?url=http://127.0.0.55:530/file',
+                        '?url=http://somedomainthatdoesnexit:530/file',
+                    ]
+                },
+            ]
+
     def testErrorPage(self):
         response = self.app.get('/clearlyshouldntexist')
         self.assertEqual(response.data, b'404') 
 
-    def testDownloaderURL(self):
-        attachments_invalid = [
-                '?',
-                '?url=',
-                '?url=',
-                '',
-                '?url=http://localhost',
-                '?url=http://localhost:5000',
-                '?url=http://127.0.0.1',
-                '?url=http://127.0.0.1:5000',
-                '/file/',
-        ]
+        response = self.app.get('/api/segment/url/fail')
+        self.assertEqual(response.data, b'404') 
 
-        for attachment in attachments_invalid:
-            response = self.app.get('/api/segment/url?url='+attachment)
-            self.assertTrue(b'URL pattern is either not valid' in response.data)
+    def testDownloaderURLGenericDisabled(self):
+        app.config['ALLOW_GENERIC_URL'] = False
+        for attachment in self.attachments_generic_urls:
+            for item in attachment['items']:
+                response = self.app.get('/api/segment/url'+item)
+                self.assertTrue(self.url_pattern_fail in bytes.decode(response.data))
 
-        attachments_noconnect = [
-                'http://somedomainthatdoesnexit/file',
-                'http://somedomainthatdoesnexit.asdf/file',
-                'http://127.168.8.55/file',
-                'http://127.168.8.55:530/file',
-                'http://somedomainthatdoesnexit:530/file',
-        ]
+    def testDownloaderURLGeneric(self):
+        app.config['ALLOW_GENERIC_URL'] = True
 
-        for attachment in attachments_noconnect:
-            response = self.app.get('/api/segment/url?url='+attachment)
-            self.assertTrue(b'Errno' in response.data)
+        for attachment in self.attachments_generic_urls:
+            for item in attachment['items']:
+                response = self.app.get('/api/segment/url'+item)
+                self.assertTrue(attachment['expect'] in bytes.decode(response.data))
 
-# Block connections to self?
+
 
 #http://localhost:5000/api/segment/url?url=https://staging.alveo.edu.au/catalog/austalk/1_114_3_8_001/document/1_114_3_8_001-ch6-speaker16.wav
 
