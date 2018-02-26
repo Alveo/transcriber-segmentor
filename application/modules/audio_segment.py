a054b61c1a6d305648e49a9b9e6c3c32722cee87
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-02-26 17:08:46 +1100

    Use new SAD function

--- application/modules/audio_segment.py
+++ application/modules/audio_segment.py
@@ -1,8 +1,7 @@
-import re
 import sndhdr
-from subprocess import Popen, PIPE, STDOUT
 
 from application import app
+from application.modules.speechtools import sad
 
 class AudioSegmentor:
     """ AudioSegmentor is a class for validating and segmenting wave files. """
@@ -25,23 +24,5 @@ class AudioSegmentor:
         return self.valid
 
     def segment(self):
-        """ Executes the external audioseg library to get SSAD information.
-
-            Returns SSAD information as JSON. """
-        command = '%s/ssad -m 1.0 -a -s -f %s %s -'
-        exe_cmd = command%('/home/audioseg/audioseg-1.2.2/src/', "16000.0", self.wave_file)
-
-        p = Popen([app.config['SSAD_PATH'],
-                '-m 1.0', '-a', '-s', '-f', '16000.0', self.wave_file, '-'],
-                stdin=PIPE, stdout=PIPE, stderr=STDOUT);
-        output = str(p.communicate()[0])
-
-        json = []
-        output = re.split(r'\\n', output)
-
-        for line in output:
-            line = re.split(r'\s{4,}', line)
-            if line[0] == "speech":
-                json += {'start': line[1], 'end': line[2]},
-
-        return json
+        """ Returns SSAD information as JSON. """
+        return sad(self.wave_file, 2)
